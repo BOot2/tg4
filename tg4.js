89a34dfeeb5c582d020b7ae4a2064b1a040252b5
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-09-10 00:01:43 +0300

    Update tg4.js

--- tg4.js
+++ tg4.js
@@ -231,7 +231,7 @@ ${prefix}join ⇏ لدخول البوت الروم
 
 
 client.on('message', function(message) {
-	const myID = "456626501182291978";
+	const myID = "307630049656438788";
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "setname")) {
 		        if(message.author.id !== myID) return;
